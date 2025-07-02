7fdf97df435978cc9d512d355f16756658d14c4f
Supraja <suprajapamula2003@gmail.com>
2025-07-02 12:52:27 +0530

    propsExample-two

--- sample-two/src/App.js
+++ sample-two/src/App.js
@@ -1,13 +1,74 @@
 
 import './App.css';
-import Header from './Header';
-import { Main } from './Main';
+import { Joke } from './Joke';
 function App() {
   return (
-    <>
-      <Header/>
-      <Main/>
-    </>
+    <div className="appSection">
+      <Joke 
+        setup="Why did the scarecrow win an award?" 
+        punchline="Because he was outstanding in his field!" 
+        jokesCount={5}
+        isFunny={true}
+        comments={[
+          {
+            id:1,
+            text:"This is a great joke!"
+          },
+          {
+            id:2,
+            text:"I love scarecrow jokes!"
+          }
+        ]}
+      />
+      <Joke 
+        setup="Why don't skeletons fight each other?" 
+        punchline="They don't have the guts!" 
+        jokesCount={3}
+        isFunny={false}
+        comments={[
+          {
+            id:1,
+            text:"Haha, that's a good one!"
+          },
+          {
+            id:2,
+            text:"Not funny at all!"
+          }
+        ]}
+      />
+      <Joke 
+        setup="Why did the math book look sad?" 
+        punchline="Because it had too many problems!" 
+        jokesCount={4}
+        isFunny={true}
+        comments={[
+          {
+            id:1,
+            text:"Math jokes are the best!"
+          },
+          {
+            id:2,
+            text:"I can relate to that!"
+          }
+        ]}
+      />
+      <Joke 
+        setup="What do you call fake spaghetti?" 
+        punchline="An impasta!" 
+        jokesCount={2}
+        isFunny={true}
+        comments={[
+          {
+            id:1,
+            text:"That's a pasta joke!"
+          },
+          {
+            id:2,
+            text:"I can't believe it's not butter!"
+          }
+        ]}
+      />
+    </div>
   );
 }
 
---
+++ sample-two/src/Joke.js
@@ -0,0 +1,13 @@
+export function Joke(props) {
+    console.log(props);
+    return(
+        <div>
+           <p>{props.setup}</p>
+           <p>{props.punchline}</p>
+           <p>{props.jokesCount}: JokesCount</p>
+           <p>{props.isFunny ? "This joke is funny!" : "This joke is not funny."}</p>
+           <p>{props.comments.forEach((comment)=> <span key={comment.id}>{}</span>)}</p>
+           <hr/>
+        </div>
+    )
+}
