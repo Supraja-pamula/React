481de57188cf57b500f30e999cf9b9dba7ad71fe
Supraja <suprajapamula2003@gmail.com>
2025-07-02 11:40:01 +0530

    props-example-two

--- sample-two/src/Main.js
+++ sample-two/src/Main.js
@@ -1,13 +1,17 @@
-
+// import { Fragment } from "react/jsx-runtime"
 export function Main(){
-    return <div className="main-content">
-        <h1>Fun facts about React!</h1>
-        <ul className="ul-list">
-            <li>Was first relese in 2013</li>
-            <li>Was originally created by Jorder</li>
-            <li>Has well over 200k stars on Github</li>
-            <li>It maintained by Meta</li>
-            <li>Powers thousands of enterprice apps, Including mobile apps</li>
-        </ul>
-    </div>
+    return(
+        <>
+            <div className="main-content">
+            <h1>Fun facts about React!</h1>
+            <ul className="ul-list">
+                <li>Was first relese in 2013</li>
+                <li>Was originally created by Jorder</li>
+                <li>Has well over 200k stars on Github</li>
+                <li>It maintained by Meta</li>
+                <li>Powers thousands of enterprice apps, Including mobile apps</li>
+            </ul>
+            </div>
+        </>
+    )
 }
