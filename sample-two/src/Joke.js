adfe1cd22ad0a3fd67a62d5b8d5dd3d11966f667
Supraja <suprajapamula2003@gmail.com>
2025-07-02 17:55:22 +0530

    updated component Jokes

--- sample-two/src/Joke.js
+++ sample-two/src/Joke.js
@@ -3,6 +3,9 @@ export function Joke(props){
     <div>
         <p>{props.setup}</p>
         <p>{props.punchline}</p>
+        <p>{props.jokesCount}: JokesCount</p>
+        <p>{props.isFunny ? "This joke is funny!" : "This joke is not funny."}</p>
+        <p>{props.comments.forEach((comment)=> <span key={comment.id}>{}</span>)}</p>
         <hr/>
     </div>
    )
