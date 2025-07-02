2f81ab2a9c2777c9319171e18d241675f591891f
Supraja <suprajapamula2003@gmail.com>
2025-07-02 14:16:09 +0530

    travel-journal-project

--- sample-two/src/App.js
+++ sample-two/src/App.js
@@ -1,10 +1,68 @@
 
 import './App.css';
 import Header from './Header';
+import { Main } from './Main';
+import america from './images/america.png';
+import china from './images/china.png';
+import india from './images/india.png';
+import japan from './images/japan.png';
 function App() {
   return (
     <>
       <Header/>
+
+      <Main 
+        image={america}
+        title="America"
+        description="A beautiful country with diverse landscapes with modern architecture."
+        list={[
+          "Known for its cultural diversity",
+          "Home to the Statue of Liberty",
+          "Famous for its national parks",
+          "Has a significant influence on global culture",
+          "Offers a wide range of cuisines"
+        ]}
+        view="https://en.wikipedia.org/wiki/United_States"
+      />
+      <Main
+        image={china}
+        title="China"
+        description="A country with a rich history and a rapidly growing economy."  
+        list={[
+          "Home to the Great Wall of China",
+          "Famous for its cuisine",
+          "Has a significant influence on global trade",
+          "Known for its technological advancements",
+          "Offers a mix of traditional and modern attractions"
+        ]}
+        view="https://en.wikipedia.org/wiki/China"
+      />
+      <Main
+        image={india}
+        title="India"
+        description="A country known for its rich history and diverse culture."
+        list={[
+          "Home to the Taj Mahal",
+          "Famous for its spices",
+          "Has a wide variety of languages",
+          "Known for its festivals",
+          "Offers a rich culinary experience"
+        ]}
+        view="https://en.wikipedia.org/wiki/India"
+      />
+      <Main
+        image={japan}
+        title="Japan"
+        description="A country known for its unique blend of tradition and modernity."
+        list={[
+          "Home to Mount Fuji",
+          "Famous for its cherry blossoms",
+          "Known for its technological innovations",
+          "Offers a rich cultural heritage",
+          "Has a unique culinary scene"
+        ]}
+        view="https://en.wikipedia.org/wiki/Japan"
+      />
     </>
   );
 }
--- sample-two/src/Footer.js
+++
@@ -1,3 +0,0 @@
-export function Footer(){
-    return <small>@2025 Ziroll developement. All rights Reserved</small>
-}
