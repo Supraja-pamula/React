9a324cda7af674f3743eeb0ef0d723eadae826bf
Supraja <suprajapamula2003@gmail.com>
2025-07-05 10:04:11 +0530

    saample

--- sample-two/src/Form.js
+++ sample-two/src/Form.js
@@ -1,8 +1,26 @@
+import React from "react";
+
 export function Form(){
+    let[ingredient,setIngredient]=React.useState([])
+    function handleSubmit(event){
+        event.preventDefault()
+        let ingredientInput=document.getElementById("ingredientInput").value;
+        setIngredient([...ingredient,ingredientInput]);
+        console.log("Ingredient",ingredient)
+    }
+  
     return(
-        <form className="form-section">
-            <input type="text" placeholder="e.g. Origano" className="inputElement"/>
-            <button className="addIngredients-btn">Add Ingredient</button>
+        <form  onSubmit={handleSubmit}>
+            <div className="form-section">
+               <input type="text" placeholder="e.g. Origano" className="inputElement" id="ingredientInput"/>
+               <button className="addIngredients-btn">Add Ingredient</button>
+            </div>
+            
+            <div className="ingredients-list">
+                <ul>
+                    {ingredient.map((item,i)=><li key={i}>{item}</li>)}
+                </ul>
+            </div>
         </form>
     )
 }
