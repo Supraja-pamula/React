9fd83a06fa54d48551e1bcbe9d9e61d960e02acd
Supraja <suprajapamula2003@gmail.com>
2025-07-05 13:27:54 +0530

    chef-claude

--- sample-two/src/Form.js
+++ sample-two/src/Form.js
@@ -2,17 +2,23 @@ import React from "react";
 
 export function Form(){
     let[ingredient,setIngredient]=React.useState([])
-    function handleSubmit(event){
-        event.preventDefault()
-        let ingredientInput=document.getElementById("ingredientInput").value;
-        setIngredient([...ingredient,ingredientInput]);
-        console.log("Ingredient",ingredient)
+    // function handleSubmit(event){
+    //     event.preventDefault()
+    //     let ingredientInput=document.getElementById("ingredientInput").value;
+    //     setIngredient([...ingredient,ingredientInput]);
+    //     console.log("Ingredient",ingredient)
+    // }
+
+    function getIngredients(formData){
+        let ingredientName=formData.get("ingredient");
+        setIngredient(prevIngredient => [...prevIngredient,ingredientName])
     }
   
     return(
-        <form  onSubmit={handleSubmit}>
+        <form action={getIngredients}>
+        {/* <form  onSubmit={handleSubmit}> */}
             <div className="form-section">
-               <input type="text" placeholder="e.g. Origano" className="inputElement" id="ingredientInput"/>
+               <input type="text" placeholder="e.g. Origano" className="inputElement" id="ingredientInput" name="ingredient"/>
                <button className="addIngredients-btn">Add Ingredient</button>
             </div>
             
