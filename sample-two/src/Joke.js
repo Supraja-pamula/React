819bc92c89cdb475eb18a610352a5f7dab5d8125
Supraja <suprajapamula2003@gmail.com>
2025-07-05 20:57:30 +0530

    updated-jokes-practice

--- sample-two/src/Joke.js
+++ sample-two/src/Joke.js
@@ -1,12 +1,25 @@
+import React from "react"
+
 export function Joke(props){
+      console.log(props)
+    let [needToShow,setNeedToShow]=React.useState(false)
+    function handleShowBtn(){
+        setNeedToShow(!needToShow);
+        console.log(needToShow  );
+      
+    }
    return (
     <div>
         <p>{props.setup}</p>
-        <p>{props.punchline}</p>
-        <p>{props.jokesCount}: JokesCount</p>
-        <p>{props.isFunny ? "This joke is funny!" : "This joke is not funny."}</p>
-        <p>{props.comments.forEach((comment)=> <span key={comment.id}>{}</span>)}</p>
-        <hr/>
+        {needToShow ? <div>
+            <p>{props.punchline}</p>
+            <p>{props.jokesCount}: JokesCount</p>
+            <p>{props.isFunny ? "This joke is funny!" : "This joke is not funny."}</p>
+            <ul>{props.comments.map((comment)=><li key={comment.id}>{comment.text}</li>)}</ul>
+        </div> : null}
+        
+        <button onClick={handleShowBtn}>{needToShow?"Hide" : "Show"} Details</button>
+        <hr/> 
     </div>
    )
 }
