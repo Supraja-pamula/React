2663dc87ab0d4a8d79f2104305011ec69d50ede8
Supraja <suprajapamula2003@gmail.com>
2025-07-07 10:45:14 +0530

    sample

--- sample-two/src/Form.js
+++ sample-two/src/Form.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-export function Form(){
-    let[ingredient,setIngredient]=React.useState([])
+export function Form() {
+    let [ingredient, setIngredient] = React.useState([])
     // function handleSubmit(event){
     //     event.preventDefault()
     //     let ingredientInput=document.getElementById("ingredientInput").value;
@@ -9,24 +9,41 @@ export function Form(){
     //     console.log("Ingredient",ingredient)
     // }
 
-    function getIngredients(formData){
-        let ingredientName=formData.get("ingredient");
-        setIngredient(prevIngredient => [...prevIngredient,ingredientName])
+    function getIngredients(formData) {
+        let ingredientName = formData.get("ingredient");
+        if (ingredientName !== "") {
+         setIngredient(prevIngredient => [...prevIngredient, ingredientName]);
+        }
+        
     }
-  
-    return(
+
+    return (
         <form action={getIngredients}>
-        {/* <form  onSubmit={handleSubmit}> */}
+            {/* <form  onSubmit={handleSubmit}> */}
             <div className="form-section">
-               <input type="text" placeholder="e.g. Origano" className="inputElement" id="ingredientInput" name="ingredient"/>
-               <button className="addIngredients-btn">Add Ingredient</button>
+                <input type="text" placeholder="e.g. Origano" className="inputElement" id="ingredientInput" name="ingredient" />
+                <button className="addIngredients-btn">Add Ingredient</button>
             </div>
-            
-            <div className="ingredients-list">
-                <ul>
-                    {ingredient.map((item,i)=><li key={i}>{item}</li>)}
-                </ul>
-            </div>
+            {ingredient.length>0 ? <section>
+                <h2>Ingredients on hand:</h2>
+                <div className="ingredients-list">
+                    <ul>
+                        {ingredient.map((item, i) => <li key={i}>{item}</li>)}
+                    </ul>
+                </div>
+                {ingredient.length >3 ? <div className="readyForRecipeSection">
+                    <div className="readyForRecipe">
+                        <div>
+                            <h4>Ready for a recipe?</h4>
+                            <p>Generate a recipe from your list of Ingredients.</p>
+                        </div>
+                        <button>Get a recipe</button>
+                    </div>
+
+                </div> :null}
+                
+            </section> : null}
+
         </form>
     )
 }
